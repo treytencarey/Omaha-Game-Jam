f67b6fb38c9fe22bdacd465552b362043c99dd60
dsnyder96 <46458387+dsnyder96@users.noreply.github.com>
2019-10-01 16:09:45 -0500

    Created basic methods for user and admin

--- Capstone/src/project/Admin.java
+++ Capstone/src/project/Admin.java
@@ -2,8 +2,11 @@ package project;
 
 public class Admin extends User {
 
-	public Admin() {
-		super();
+	public Admin(String x) {
+		super(x);
 	}
-
+	
+	public void setUserPermissions(String id, int pernum) {
+		//Set account permissions of id to pernum
+	}
 }
--- Capstone/src/project/User.java
+++ Capstone/src/project/User.java
@@ -2,4 +2,15 @@ package project;
 
 public class User {
 
+	String id;
+	
+	public User(String x) {
+		
+		id = x;
+	}
+	
+	@Override
+	public String toString() {
+		return "User Information:\n\nUser ID:  "+id;
+	}
 }
