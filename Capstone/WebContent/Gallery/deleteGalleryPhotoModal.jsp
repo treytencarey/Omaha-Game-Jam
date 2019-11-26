65fc3daa4156d552c63b554a6286ea6585aea8d7
brcrossley <bcrossley@unomaha.edu>
2019-11-26 14:37:06 -0600

    Added photo deletion for Gallery

--- Capstone/WebContent/Gallery/deleteGalleryPhotoModal.jsp
+++ Capstone/WebContent/Gallery/deleteGalleryPhotoModal.jsp
@@ -11,7 +11,7 @@
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 			</div>
 			<div class="modal-body">
-				<form class="was-validated" action="<%=request.getContextPath()%>/GalleryServlet" method="post">
+				<form class="was-validated" action="<%=request.getContextPath()%>/GalleryServlet" method="post" enctype="multipart/form-data">
 					<input type="text" id="galleryFile" name="galleryFile" hidden/>
 					<input type="text" id="galleryEvent" name="galleryEvent" hidden/>
 					<button type="submit" id="deleteGalleryPhotosButton" name="deleteGalleryPhotosButton" class="btn btn-danger btn-block btn-lg" style="background-color: red;">Delete</button>
--- Capstone/src/servlets/GalleryServlet.java
+++ Capstone/src/servlets/GalleryServlet.java
@@ -31,7 +31,6 @@ import project.Main;
 public class GalleryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String DESTINATION_JSP = "Gallery/index.jsp";
-
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -95,28 +94,44 @@ public class GalleryServlet extends HttpServlet {
 		}
 		
 		if (request.getParameter("deleteGalleryPhotosButton") != null) {
-			
+			String fileToDelete = request.getParameter("galleryFile");
+			deleteFile(fileToDelete, eventPKey);
 		}
 
 		response.sendRedirect(redirectLink);
 	}
 
+	/**
+	 * Saves the given file in a directory on the server (Uploads/Gallery)
+	 * @param p - Part to get input stream from
+	 * @param filename - name of the file to be saved into directory
+	 * @param pKey - specifies the PKey of the event. Event PKey is the name of folder that file is saved in
+	 * @throws IOException
+	 */
 	private static void saveFile(Part p, String filename, int pKey) throws IOException {
-		// String path = "/Uploads/Gallery/" + pKey + "/" + pKey;
-		//String path = "/Uploads/Gallery/Gallery";
-		//String[] splits = path.replaceAll("\\", "/").split("/");
-		//String n = splits[splits.length - 1];
 		String path = Main.context.getRealPath("/Uploads/Gallery/");
-		path = path + pKey + "/" + filename;
+		path = path + pKey + "\\" + filename;
 		
 		Path realPath = Paths.get(path);
 
 		try (InputStream is = p.getInputStream()) {
 			if(!Files.exists(realPath)) {
-				System.out.println(realPath);
+				//System.out.println(realPath);
 				Files.createDirectories(realPath);
 			}
 			Files.copy(is, realPath, REPLACE_EXISTING);
 		}
+	}
+	
+	/**
+	 * Deletes a file from server directory based on given filename and pKey
+	 * @param filename - name of the file to be deleted from directory
+	 * @param pKey - specifies the PKey of the event. Event PKey is the name of folder that file is saved in
+	 */
+	private static void deleteFile(String filename, int pKey) {
+		String path = Main.context.getRealPath("/Uploads/Gallery/");
+		path = path + pKey + "\\" + filename;
+		System.out.println(path);
+		new File(path).delete();
 	}
 }
