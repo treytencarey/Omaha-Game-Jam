9d2a412f46e755b4016c1e4785574cea55cc96b1
brcrossley <bcrossley@unomaha.edu>
2019-11-30 13:53:37 -0600

    Refactored NewsServlet

--- Capstone/src/servlets/NewsServlet.java
+++ Capstone/src/servlets/NewsServlet.java
@@ -85,6 +85,7 @@ public class NewsServlet extends HttpServlet {
 		String reqTitle = request.getParameter("newsTitle");
 		String reqHeader = request.getParameter("newsHeader");
 		String reqBody = request.getParameter("newsBody");
+		String pKey = null;
 		boolean isPublicChecked = request.getParameter("isPublicCheckbox") != null;
 
 		/**
@@ -95,65 +96,30 @@ public class NewsServlet extends HttpServlet {
 			isPublicDb = 1;
 		else
 			isPublicDb = 0;
-
+		
 		/**
-		 * Format the date to MM/dd/yyyy to display in database/page
+		 * Get Part/image from form in each (exception occurs if getPart is used up here with the delete function)
 		 */
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-		String currentDate = dtf.format(LocalDateTime.now());
-
-		List<Map<String, Object>> query = null;
-
-		/**
-		 * Find the path where the body txt file will go
-		 */
-		path = getServerPath("/Uploads/News/Body/Body");
+		Part headerImg = null;
 
 		if (request.getParameter("newNewsArticleButton") != null) {
-			Database.executeUpdate("INSERT OR REPLACE INTO Blogs(Date, Title, IsPublic, Header) VALUES (\'"
-					+ currentDate + "\', \'" + reqTitle + "\', \'" + isPublicDb + "\', \'" + reqHeader + "\')");
-			query = Database.executeQuery(
-					"SELECT PKey FROM Blogs WHERE Date=\'" + currentDate + "\' AND Title=\'" + reqTitle + "\'");
+			if (!request.getPart("newsFile").getSubmittedFileName().isEmpty())
+				headerImg = request.getPart("newsFile");
+			pKey = addNewsArticle(reqTitle, reqHeader, reqBody, isPublicDb, headerImg);
 		}
 
 		if (request.getParameter("editNewsArticleButton") != null) {
 			String id = request.getParameter("newsId");
-			Database.executeUpdate("UPDATE Blogs SET Date=\'" + currentDate + "\', Title=\'" + reqTitle
-					+ "\', IsPublic=\'" + isPublicDb + "\', Header=\'" + reqHeader + "\' WHERE PKey=" + id);
-			query = Database.executeQuery("SELECT PKey FROM Blogs WHERE Pkey=" + id);
+			if (!request.getPart("newsFile").getSubmittedFileName().isEmpty())
+				headerImg = request.getPart("newsFile");
+			pKey = editNewsArticle(id, reqTitle, reqHeader, reqBody, isPublicDb, headerImg);
 		}
 
 		if (request.getParameter("deleteNewsArticleButton") != null) {
 			String id = request.getParameter("newsId");
-			Database.executeUpdate("DELETE FROM Blogs WHERE PKey=" + id);
-			Files.deleteIfExists(Paths.get(path + id + "_body.txt"));
-			path = getServerPath("/Uploads/News/Photo/Photo");
-			Files.deleteIfExists(Paths.get(path + id + "_header.png"));
+			deleteNewsArticle(id);
 			response.sendRedirect(redirectLink);
 			return;
-		}
-
-		/**
-		 * Create the file in the form of PKey_body.txt and write/overwrite to the file
-		 */
-		String pKey = query.get(0).get("PKey").toString();
-		File file = new File(path + pKey + "_body.txt");
-		file.createNewFile();
-		FileOutputStream outFile = new FileOutputStream(file);
-		outFile.write(reqBody.getBytes());
-		outFile.close();
-
-		/**
-		 * Get the image from newsFile in form, and write to Upload/News/Photo
-		 */
-		if (!request.getPart("newsFile").getSubmittedFileName().isEmpty()) {
-			Part headerImg = request.getPart("newsFile");
-			path = getServerPath("/Uploads/News/Photo/Photo");
-			Path imgPath = FileSystems.getDefault().getPath(path, pKey + "_header.png");
-
-			try (InputStream is = headerImg.getInputStream()) {
-				Files.copy(is, imgPath, REPLACE_EXISTING);
-			}
 		}
 
 		/**
@@ -162,6 +128,96 @@ public class NewsServlet extends HttpServlet {
 		 */
 		redirectLink = redirectLink + "/view?id=" + pKey;
 		response.sendRedirect(redirectLink);
+	}
+
+	public static String addNewsArticle(String title, String subtitle, String body, int isPublic, Part headerImg) throws IOException {
+		List<Map<String, Object>> query = null;
+		/**
+		 * Format the date to MM/dd/yyyy to display in database/page
+		 */
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		String currentDate = dtf.format(LocalDateTime.now());
+
+		/**
+		 * Find the path where the body txt file will go
+		 */
+		String path = getServerPath("/Uploads/News/Body/Body");
+
+		Database.executeUpdate("INSERT OR REPLACE INTO Blogs(Date, Title, IsPublic, Header) VALUES (\'" + currentDate
+				+ "\', \'" + title + "\', \'" + isPublic + "\', \'" + subtitle + "\')");
+		query = Database.executeQuery(
+				"SELECT PKey FROM Blogs WHERE Date=\'" + currentDate + "\' AND Title=\'" + title + "\'");
+		
+		String pKey = query.get(0).get("PKey").toString();
+		createFile(pKey, path, body);
+		
+		/**
+		 * Get the part(image) and write to Upload/News/Photo
+		 */
+		if(headerImg != null) {
+			path = getServerPath("/Uploads/News/Photo/Photo");
+			Path imgPath = FileSystems.getDefault().getPath(path, pKey + "_header.png");
+
+			try (InputStream is = headerImg.getInputStream()) {
+				Files.copy(is, imgPath, REPLACE_EXISTING);
+			}
+		}
+		
+		return pKey;
+	}
+	
+	public static String editNewsArticle(String id, String title, String subtitle, String body, int isPublic, Part headerImg) throws IOException {
+		List<Map<String, Object>> query = null;
+		/**
+		 * Format the date to MM/dd/yyyy to display in database/page
+		 */
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		String currentDate = dtf.format(LocalDateTime.now());
+
+		/**
+		 * Find the path where the body txt file will go
+		 */
+		String path = getServerPath("/Uploads/News/Body/Body");
+
+		Database.executeUpdate("UPDATE Blogs SET Date=\'" + currentDate + "\', Title=\'" + title
+				+ "\', IsPublic=\'" + isPublic + "\', Header=\'" + subtitle + "\' WHERE PKey=" + id);
+		query = Database.executeQuery("SELECT PKey FROM Blogs WHERE Pkey=" + id);
+		
+		String pKey = query.get(0).get("PKey").toString();
+		createFile(pKey, path, body);
+		
+		/**
+		 * Get the part(image) and write to Upload/News/Photo
+		 */
+		if(headerImg != null) {
+			path = getServerPath("/Uploads/News/Photo/Photo");
+			Path imgPath = FileSystems.getDefault().getPath(path, pKey + "_header.png");
+
+			try (InputStream is = headerImg.getInputStream()) {
+				Files.copy(is, imgPath, REPLACE_EXISTING);
+			}
+		}
+		
+		return pKey;
+	}
+	
+	public static void deleteNewsArticle(String id) throws IOException {
+		String path = getServerPath("/Uploads/News/Body/Body");
+		Database.executeUpdate("DELETE FROM Blogs WHERE PKey=" + id);
+		Files.deleteIfExists(Paths.get(path + id + "_body.txt"));
+		path = getServerPath("/Uploads/News/Photo/Photo");
+		Files.deleteIfExists(Paths.get(path + id + "_header.png"));
+	}
+	
+	private static void createFile(String pKey, String path, String body) throws IOException {
+		/**
+		 * Create the file in the form of PKey_body.txt and write/overwrite to the file
+		 */
+		File file = new File(path + pKey + "_body.txt");
+		file.createNewFile();
+		FileOutputStream outFile = new FileOutputStream(file);
+		outFile.write(body.getBytes());
+		outFile.close();
 	}
 
 	private static String getServerPath(String orig) {
