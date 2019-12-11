58a5e43b73a69bcecf8cb583217c705cb57bf378
Aaron Frickel <africkel@unomaha.edu>
2019-12-10 23:43:35 -0600

    Clean up various prints and remove potentially error-producing @Override 's from ActivityStashWriter

--- Capstone/src/weblisteners/ActivityStashWriter.java
+++ Capstone/src/weblisteners/ActivityStashWriter.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-import project.Main;
-
 /**
  * Listens for Session expiration and appends its attributes to activities.csv.
  */
@@ -22,44 +20,34 @@ public class ActivityStashWriter implements HttpSessionListener {
 	private static final String VIRTUAL_CSV_PATH = "Analytics/activities.csv"; // Path to CSV file from realPath (WebContent I think)
 	private static final String CSV_HEADER_LINE = "AccountPKey,Referer,Landing,RSVPdEventPKey,AccessDate\n"; // Header line to begin the CSV file with in case CSV doesn't exist.
 	private BufferedWriter bw; // Singleton that all ActivityStashWriters use (but it looks like there's only 1 instantiated based on my tests)
-	
-	public ActivityStashWriter() {
-		// TODO Auto-generated constructor stub
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nActivityStashWriter CREATED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! YAYAYAYAYAYAYAY\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-	}
-	
-	@Override
-	public void sessionCreated(HttpSessionEvent se) {
-		System.out.println("Session created!"); // Just to show this class is listening, don't actually need this method overridden.
-	}
 
 	/**
 	 * Once a Session is about to expire, collects its attributes and stores them in activities.csv. If that file doesn't exist, create it and write its headers.
 	 */
-	@Override
 	public void sessionDestroyed(HttpSessionEvent se)
 	{
-		System.out.println("Session destroyed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		try
 		{
 			HttpSession session = se.getSession();
 
+			// Retrieve attributes from Session
 			String AccountPKey = formatAttributeForCSV(session.getAttribute("accountPKey"));
 			String Referer = formatAttributeForCSV(session.getAttribute("Referer"));
 			String Landing = formatAttributeForCSV(session.getAttribute("Landing"));
 			String RSVPdEventPKey = formatAttributeForCSV(session.getAttribute("RSVPdEventPKey"));
 			String AccessDate = formatAttributeForCSV(session.getAttribute("AccessDate"));
 			
+			// Print out the attributes
 			System.out.println("Session expired! Appending to activities.csv...");
 			System.out.println("Referer: " + Referer);
 			System.out.println("Landing: " + Landing);
 			System.out.println("AccountPKey: " + AccountPKey);
 			System.out.println("RSVPdEventPKey: " + RSVPdEventPKey);
 			System.out.println("AccessDate: " + AccessDate);
-			System.out.println("from Main: " + Main.context.getRealPath(VIRTUAL_CSV_PATH));
+			
+			// Prepare activities.csv
 			File af = new File(session.getServletContext().getRealPath(VIRTUAL_CSV_PATH));
 			System.out.println(af.getAbsolutePath());
-//			System.out.println(af.exists());
 			if (af.exists()) // If CSV already exists: just initialize bw
 			{
 				FileWriter fw = new FileWriter(af, true);
@@ -73,6 +61,7 @@ public class ActivityStashWriter implements HttpSessionListener {
 				bw.write(CSV_HEADER_LINE);
 			}
 			
+			// Append attributes to activities.csv
 			bw.append(String.format("%s,%s,%s,%s,%s\n", AccountPKey, Referer, Landing, RSVPdEventPKey, AccessDate)); // Append this user's Session attributes
 			bw.flush();
 		}
@@ -100,4 +89,13 @@ public class ActivityStashWriter implements HttpSessionListener {
 		result.replace(",", "\",\""); // Wrap all commas in double quotes
 		return result;
 	}
+	
+//	public ActivityStashWriter() {
+//		// TODO Auto-generated constructor stub
+//		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nActivityStashWriter CREATED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! YAYAYAYAYAYAYAY\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+//	}
+//	
+//	public void sessionCreated(HttpSessionEvent se) {
+//		System.out.println("Session created!"); // Just to show this class is listening, don't actually need this method overridden.
+//	}
 }
